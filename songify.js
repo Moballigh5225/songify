4458f77af3f0a7859d14f6ccbd5e43b68a486b8c
Moballighul islam <moballigh5225@gmail.com>
2023-01-19 02:38:46 +0530

    ui part developed

---
+++ songify.js
@@ -0,0 +1,9 @@
+console.log("welcome to the spotify");
+
+//Initialize the Variable
+let songIndex = 0;
+let audioElement = new Audio("1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+
+// audioElement.play();
